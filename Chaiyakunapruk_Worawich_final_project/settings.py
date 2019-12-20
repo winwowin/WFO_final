58e440b69a415c12d0cd949eba1bf9c5483473d1
winwowin <winwowin@gmail.com>
2019-12-20 02:22:41 -0600

    Deployment fixed

--- Chaiyakunapruk_Worawich_final_project/settings.py
+++ Chaiyakunapruk_Worawich_final_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'hrofn)vjqq0qlatsd=j8ha)f5g7u*e+b-$-2r^a6pu!uyapb#f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://winwowin.pythonanywhere.com/']
+ALLOWED_HOSTS = ['winwowin.pythonanywhere.com']
 
 
 # Application definition
